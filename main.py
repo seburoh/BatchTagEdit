6651408c690e00b9e751fa4e8a718b29b471fe9e
seburoh <82007558+seburoh@users.noreply.github.com>
2021-04-22 17:11:22 -0700

    removing depreciated files

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
+from mutagen.oggvorbis import OggVorbis
+from mutagen.mp4 import MP4
 
 # apply metadata to FLAC audio files based on specific filename structure
 # example filename: 07. M-Project - Pure Powerstomper (feat. Jonjo) [(Refix)].flac
 
-# load folder to apologize to later
-basefolder = 'D:\\mega\\'
+# load folder
+basefolder = 'D:\\folder'
 
 with os.scandir(basefolder) as folder:
 
     # specify track being worked on
     for track in folder:
         if track.is_file():
-
-            # debug print me the track filename
-            print(track.name)
 
             # make filename into nice string
             filename = track.name
@@ -33,15 +33,32 @@ with os.scandir(basefolder) as folder:
             trtype = trdata[1]
 
             # debug print me the strings
-            print("#:", trnumber, "artist:", trartist, "title:", trtitle)
+            print("#:", trnumber, "artist:", trartist, "title:", trtitle, "filetype:", trtype)
 
             # mutagen me captain
             trpath = basefolder + "\\" + track.name
 
-            trfile = FLAC(trpath)
+            # understand filetype
+            skipvar = False
+            if trtype == "flac":
+                trfile = FLAC(trpath)
+            if trtype == "ogg":
+                trfile = OggVorbis(trpath)
+            if trtype == "mp3":
+                trfile = MP3(trpath)
+                skipvar = True
+            if trtype == "m4a":
+                trfile = MP4(trpath)
+                skipvar = True
+            if trtype == "wav":
+                skipvar = True
 
             # apply mutagen to the problem
-            trfile["tracknumber"] = trnumber
-            trfile["title"] = trtitle
-            trfile["artist"] = trartist
-            trfile.save()
+            if not skipvar:
+                trfile["tracknumber"] = trnumber
+                trfile["title"] = trtitle
+                trfile["artist"] = trartist
+                trfile.save()
+                print("Track data saved")
+            else:
+                print("Track skipped")
